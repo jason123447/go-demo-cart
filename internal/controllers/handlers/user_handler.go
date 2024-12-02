dd37c2cc45717fd30d632764e953bfdd78e54909
Lenny bai <>
2024-12-02 16:55:19 +0800

    remove reduntant code

--- internal/controllers/handlers/user_handler.go
+++ internal/controllers/handlers/user_handler.go
@@ -10,7 +10,6 @@ import (
 
 func GetUserHandler(c *gin.Context) {
 	userID, _ := strconv.Atoi(c.Param("id"))
-	services.GetUserSvc(userID)
 	user, err := services.GetUserSvc(userID)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})

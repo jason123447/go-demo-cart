c77a99e74605523cc1dd896a966c2d0bb9e7b5a5
Lenny bai <>
2024-12-06 12:20:14 +0800

    [FE] fix 使用固定user fetch api

--- frontend/shopping-cart/src/app/components/mall/cart/cart.component.ts
+++ frontend/shopping-cart/src/app/components/mall/cart/cart.component.ts
@@ -5,10 +5,10 @@ import { MatRippleModule } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedDirectiveModule } from '../../../directive/shared-directive.module';
-import { UserService } from '../../../services/user.service';
 import { DataService } from '../../../services/data/data.service';
 import { LayoutService } from '../../../services/layout.service';
 import { lastValueFrom } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-cart',
@@ -19,7 +19,7 @@ import { lastValueFrom } from 'rxjs';
 })
 export class CartComponent {
   cartServ = inject(CartService);
-  userServ = inject(UserService);
+  authServ = inject(AuthService);
   dataServ = inject(DataService);
   layoutServ = inject(LayoutService);
   total: number = 0.00;
@@ -50,7 +50,7 @@ export class CartComponent {
   async onClickedCheckout() {
     this.layoutServ.appLoading = true;
     await lastValueFrom(this.dataServ.postOrder({
-      user_id: this.userServ.user!.id,
+      user_id: this.authServ.user!.id,
       order_items: this.cartItems.map(e => (
         {
           product_id: e.id,

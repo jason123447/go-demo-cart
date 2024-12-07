cba8aac301183bf7f8ee37df440dbf00b3d0baed
Lenny bai <>
2024-12-07 20:06:56 +0800

    [BE] remove reduntant code

--- internal/controllers/ctrls.go
+++ internal/controllers/ctrls.go
@@ -73,23 +73,3 @@ func SetControllers(r *gin.Engine) {
 		c.Writer.Write([]byte(stringBuilder.String()))
 	})
 }
-
-// type Controller struct {
-//     Ct        *gin.Context
-//     Data      map[interface{}]interface{}
-//     ChildName string
-// }
-
-// type ControllerInterface interface {
-// 	// Init(ct *gin.Context, cn string) //初始化上下文和子類別名稱稱
-// 	Prepare()      //開始執行之前的一些處理
-// 	Get()          //method=GET 的處理
-// 	Post()         //method=POST 的處理
-// 	Delete()       //method=DELETE 的處理
-// 	Put()          //method=PUT 的處理
-// 	Head()         //method=HEAD 的處理
-// 	Patch()        //method=PATCH 的處理
-// 	Options()      //method=OPTIONS 的處理
-// 	Finish()       //執行完成之後的處理
-// 	Render() error //執行完 method 對應的方法之後渲染頁面
-// }

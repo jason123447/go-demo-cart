495ed1faf7778c8c5a459e9f4721fbd09572d9b4
Lenny bai <>
2024-12-09 14:48:37 +0800

    for web service deploy

--- internal/db/database.go
+++ internal/db/database.go
@@ -10,7 +10,12 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=127.0.0.1 user=admin password=admin dbname=go_demo_db port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	/**
+	 * host=db
+	 * db is the name of the service in docker-compose.yml (docker-compose alias)
+	 * use hsot=127.0.0.1 for local development
+	 */
+	dsn := "host=db user=admin password=admin dbname=go_demo_db port=5432 sslmode=disable TimeZone=Asia/Taipei"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)

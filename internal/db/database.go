f13fe63e7fdffc08dd23d13d494c5952830e43c7
Lenny bai <>
2024-12-09 16:14:32 +0800

    deploy api

--- internal/db/database.go
+++ internal/db/database.go
@@ -15,7 +15,19 @@ func InitDB() {
 	 * db is the name of the service in docker-compose.yml (docker-compose alias)
 	 * use hsot=127.0.0.1 for local development
 	 */
-	dsn := "host=https://go-demo-cart-db.onrender.com user=admin password=admin dbname=go_demo_db port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	// 讀取 init.sql 檔案
+	// sqlFile, err := os.Open("init.sql")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer sqlFile.Close()
+
+	// sqlBytes, err := ioutil.ReadAll(sqlFile)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	dsn := "postgresql://admin:U4TvHRgMMTsWmG7Po0nWARLNYUgJMzgI@dpg-ctb9mnt2ng1s73dqs990-a.singapore-postgres.render.com/go_demo_db"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
